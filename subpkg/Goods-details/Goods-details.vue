96e4d15096a88d371ac3a619d8a3de278d777e08
cartonist <3318279402@qq.com>
2023-02-19 22:27:12 +0800

    完成了商品详情页的开发

--- subpkg/Goods-details/Goods-details.vue
+++ subpkg/Goods-details/Goods-details.vue
@@ -1,19 +1,166 @@
 <template>
-	<view>
-		111
+	<!-- 在页面数据还没请求来之前先不展示对应数据 -->
+	<view v-if="goodsInfo.goods_name" class="goods-details-container">
+		<!-- 轮播图区域 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="(item, index) in goodsInfo.pics" :key="index">
+				<image :src="item.pics_big" mode="" @click="previewImg(index)"></image>
+			</swiper-item>
+		</swiper>
+		<view class="goods-message">
+			<view class="goods-price">
+				￥{{goodsInfo.goods_price.toFixed(2)}}
+			</view>
+			<view class="goods-details">
+				<view class="goods-name">
+					{{goodsInfo.goods_name}}
+				</view>
+				<view class="goods-fav">
+					<uni-icons type="star" :size="18" color="gray"></uni-icons>
+					<text>收藏</text>
+				</view>
+			</view>
+		</view>
+		<view class="express">
+			快递：免运费
+		</view>
+		<rich-text :nodes="goodsInfo.goods_introduce"></rich-text>
+		<!-- 使用uniapp的组件实现底部nav-bar -->
+		<view class="nav-bar">
+			<uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="goTOCart"
+				@buttonClick="buttonClick" />
+		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-
-			};
+<script setup>
+	import {
+		ref
+	} from "vue";
+	import {
+		onLoad
+	} from "@dcloudio/uni-app"
+	// 商品详情数据
+	const goodsInfo = ref({})
+	const options = ref([{
+		icon: 'shop',
+		text: '店铺',
+		infoBackgroundColor: '#007aff',
+		infoColor: "red"
+	}, {
+		icon: 'cart',
+		text: '购物车',
+		info: 2
+	}])
+	const buttonGroup = ref([{
+			text: '加入购物车',
+			backgroundColor: '#ff0000',
+			color: '#fff'
+		},
+		{
+			text: '立即购买',
+			backgroundColor: '#ffa200',
+			color: '#fff'
+		}
+	])
+	onLoad((option) => {
+		const goodsId = option.goods_id
+		getGoodsInfo(goodsId)
+	})
+	// 获取商品详情数据
+	const getGoodsInfo = async (goodsId) => {
+		const {
+			data: res
+		} = await uni.$http.get('/api/public/v1/goods/detail', {
+			goods_id: goodsId
+		})
+		if (res.meta.status == 200) {
+			goodsInfo.value = res.message
+			// goodsInfo.value.price = goodsInfo.value.goods_price.toFixed(2)
+			// 使用字符串的replace方法将富文本中的<img 替换为 <img style="display: block"
+			goodsInfo.value.goods_introduce = goodsInfo.value.goods_introduce.replace(/<img/g,
+				'<img style="display: block;"').replace(/webp/g, 'jpg')
+		}
+	}
+	// 轮播图预览
+	const previewImg = (index) => {
+		uni.previewImage({
+			// 预览是默认显示图片的索引
+			current: index,
+			// 所有图片url地址的数组
+			urls: goodsInfo.value.pics.map(x => x.pics_big)
+		})
+	}
+	// 跳转到购物车页面
+	const goTOCart = (e) => {
+		if (e.content.text == '购物车') {
+			uni.switchTab({
+				url: '/pages/Cart/Cart'
+			})
 		}
 	}
 </script>
 
 <style lang="less" scoped>
+	.goods-details-container {
+		padding-bottom: 50px
+	}
 
+	swiper {
+		height: 750rpx;
+
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	.goods-message {
+		padding: 10px 0 10px 10px;
+		background: #fff;
+
+		.goods-price {
+			font-size: 20px;
+			margin-bottom: 10px;
+			color: #d81e06;
+
+		}
+
+		.goods-details {
+			display: flex;
+			justify-content: space-between;
+
+			.goods-name {
+				font-size: 14px;
+				margin-right: 15px;
+			}
+
+			.goods-fav {
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				width: 100px;
+				font-size: 14px;
+				border-left: 1px solid #efefef;
+				color: gray;
+			}
+
+		}
+	}
+
+	.express {
+		font-size: 12px;
+		color: gray;
+		padding-left: 10px;
+		background: #fff;
+	}
+
+	.nav-bar {
+		width: 100%;
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		z-index: 999;
+	}
 </style>
--- unpackage/dist/dev/mp-weixin/subpkg/Goods-details/Goods-details.js
+++ unpackage/dist/dev/mp-weixin/subpkg/Goods-details/Goods-details.js
@@ -1,12 +1,101 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
+if (!Array) {
+  const _easycom_uni_icons2 = common_vendor.resolveComponent("uni-icons");
+  const _easycom_uni_goods_nav2 = common_vendor.resolveComponent("uni-goods-nav");
+  (_easycom_uni_icons2 + _easycom_uni_goods_nav2)();
+}
+const _easycom_uni_icons = () => "../../uni_modules/uni-icons/components/uni-icons/uni-icons.js";
+const _easycom_uni_goods_nav = () => "../../uni_modules/uni-goods-nav/components/uni-goods-nav/uni-goods-nav.js";
+if (!Math) {
+  (_easycom_uni_icons + _easycom_uni_goods_nav)();
+}
 const _sfc_main = {
-  data() {
-    return {};
+  __name: "Goods-details",
+  setup(__props) {
+    const goodsInfo = common_vendor.ref({});
+    const options = common_vendor.ref([{
+      icon: "shop",
+      text: "\u5E97\u94FA",
+      infoBackgroundColor: "#007aff",
+      infoColor: "red"
+    }, {
+      icon: "cart",
+      text: "\u8D2D\u7269\u8F66",
+      info: 2
+    }]);
+    const buttonGroup = common_vendor.ref([
+      {
+        text: "\u52A0\u5165\u8D2D\u7269\u8F66",
+        backgroundColor: "#ff0000",
+        color: "#fff"
+      },
+      {
+        text: "\u7ACB\u5373\u8D2D\u4E70",
+        backgroundColor: "#ffa200",
+        color: "#fff"
+      }
+    ]);
+    common_vendor.onLoad((option) => {
+      const goodsId = option.goods_id;
+      getGoodsInfo(goodsId);
+    });
+    const getGoodsInfo = async (goodsId) => {
+      const {
+        data: res
+      } = await common_vendor.index.$http.get("/api/public/v1/goods/detail", {
+        goods_id: goodsId
+      });
+      if (res.meta.status == 200) {
+        goodsInfo.value = res.message;
+        goodsInfo.value.goods_introduce = goodsInfo.value.goods_introduce.replace(
+          /<img/g,
+          '<img style="display: block;"'
+        ).replace(/webp/g, "jpg");
+      }
+    };
+    const previewImg = (index) => {
+      common_vendor.index.previewImage({
+        current: index,
+        urls: goodsInfo.value.pics.map((x) => x.pics_big)
+      });
+    };
+    const goTOCart = (e) => {
+      if (e.content.text == "\u8D2D\u7269\u8F66") {
+        common_vendor.index.switchTab({
+          url: "/pages/Cart/Cart"
+        });
+      }
+    };
+    return (_ctx, _cache) => {
+      return common_vendor.e({
+        a: goodsInfo.value.goods_name
+      }, goodsInfo.value.goods_name ? {
+        b: common_vendor.f(goodsInfo.value.pics, (item, index, i0) => {
+          return {
+            a: item.pics_big,
+            b: common_vendor.o(($event) => previewImg(index), index),
+            c: index
+          };
+        }),
+        c: common_vendor.t(goodsInfo.value.goods_price.toFixed(2)),
+        d: common_vendor.t(goodsInfo.value.goods_name),
+        e: common_vendor.p({
+          type: "star",
+          size: 18,
+          color: "gray"
+        }),
+        f: goodsInfo.value.goods_introduce,
+        g: common_vendor.o(goTOCart),
+        h: common_vendor.o(_ctx.buttonClick),
+        i: common_vendor.p({
+          fill: true,
+          options: options.value,
+          buttonGroup: buttonGroup.value
+        })
+      } : {});
+    };
   }
 };
-function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {};
-}
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/Program Download/miniprogram/uni-shop/subpkg/Goods-details/Goods-details.vue"]]);
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__scopeId", "data-v-3bd2e652"], ["__file", "D:/Program Download/miniprogram/uni-shop/subpkg/Goods-details/Goods-details.vue"]]);
 wx.createPage(MiniProgramPage);
